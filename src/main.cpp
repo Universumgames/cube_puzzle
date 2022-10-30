9219df4cab01fc4a8ebcc2d8f6011d3da7922332
Universumgames <programming@universegame.de>
2022-10-30 18:53:07 +0100

    merge from jan's base

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
-int main(int argc, char *argv[])
+#include "global.h"
+
+#include "examplegame.h"
+
+extern "C"
+int main( int argc, char* argv[] )
+try
 {
-    return 0;
+	ExampleGame game;
+	return game.Run();
+}
+catch( ... )
+{
+	std::cout << "Non-SDL Exception has occured!" << std::endl;
+
+	// put a breakpoint in this line, to prevent the shell from closing
+	return 66;
 }
