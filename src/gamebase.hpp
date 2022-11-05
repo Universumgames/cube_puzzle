8b928545519e8c337085d1860780e838b22bed75
Universumgames <programming@universegame.de>
2022-11-05 10:09:48 +0100

    reformatting

--- src/gamebase.hpp
+++ src/gamebase.hpp
@@ -1,94 +1,101 @@
 #pragma once
 
 #include "global.hpp"
+#include "data/SpriteStorage.hpp"
 
 class Game;
+
 class GameState;
 
-class Game
-{
+class Game {
 protected:
-	Window *window;
-	Renderer *render;
+    Window *window;
+    Renderer *render;
 
-	bool isRunning = true;
-	u32 frame = 0;
+    bool isRunning = true;
+    u32 frame = 0;
 
-	int currentStateIdx = -1;
-	int nextStateIdx = -1;
+    int currentStateIdx = -1;
+    int nextStateIdx = -1;
 
-	GameState *currentState = nullptr;
-	Vector<GameState *> allStates;
+    GameState *currentState = nullptr;
+    Vector<GameState *> allStates;
 
     Point windowSize;
 
 public:
-	[[nodiscard]] bool IsRunning() const { return isRunning; }
+    [[nodiscard]] bool IsRunning() const { return isRunning; }
 
-	explicit Game(
-		const char *windowTitle = "SDL Game",
-		const Point windowSize = Point{1024, 720},
-		const bool vSync = true);
-	virtual ~Game();
+    explicit Game(
+            const char *windowTitle = "SDL Game",
+            const Point windowSize = Point{1024, 720},
+            const bool vSync = true);
 
-	virtual bool HandleEvent(const Event event);
-	virtual int Run();
+    virtual ~Game();
 
-	virtual void SetNextState(int index) { nextStateIdx = index; }
+    virtual bool HandleEvent(const Event event);
 
-	Point getWindowSize();
+    virtual int Run();
+
+    virtual void SetNextState(int index) { nextStateIdx = index; }
+
+    Point getWindowSize();
+
 
 protected:
-	virtual void ActivateNextState();
+    virtual void ActivateNextState();
 
-	/// Performance
+    /// Performance
 public:
-	// All output msec per frame, see: http://renderingpipeline.com/2013/02/fps-vs-msecframe/
-	enum class PerformanceDrawMode
-	{
-		None,
-		Title,	 // averaged and only updated every 250ms
-		OStream, // averaged and only updated every 1000ms
-	};
+    // All output msec per frame, see: http://renderingpipeline.com/2013/02/fps-vs-msecframe/
+    enum class PerformanceDrawMode {
+        None,
+        Title,     // averaged and only updated every 250ms
+        OStream, // averaged and only updated every 1000ms
+    };
 
-	void SetPerfDrawMode(PerformanceDrawMode mode) noexcept { perfDrawMode = mode; }
+    void SetPerfDrawMode(PerformanceDrawMode mode) noexcept { perfDrawMode = mode; }
 
 protected:
-	PerformanceDrawMode perfDrawMode = PerformanceDrawMode::None;
-	u32 lastPerfInfoFrame = 0;
-	TimePoint lastPerfInfoTime = Clock::now();
-	Duration accumulatedNeeded = Duration::zero();
+    PerformanceDrawMode perfDrawMode = PerformanceDrawMode::None;
+    u32 lastPerfInfoFrame = 0;
+    TimePoint lastPerfInfoTime = Clock::now();
+    Duration accumulatedNeeded = Duration::zero();
 
-	[[nodiscard]] float AverageMSecPerFrame() const;
-	void ResetPerformanceInfo(const TimePoint current);
-	void OutputPerformanceInfo(const TimePoint current, const Duration needed);
+    [[nodiscard]] float AverageMSecPerFrame() const;
+
+    void ResetPerformanceInfo(const TimePoint current);
+
+    void OutputPerformanceInfo(const TimePoint current, const Duration needed);
 };
 
-class GameState
-{
+class GameState {
 protected:
-	Game &game;
-	Renderer *render;
+    Game &game;
+    Renderer *render;
 
 public:
-	GameState(Game &game, Renderer *render);
-	virtual ~GameState() = default;
+    GameState(Game &game, Renderer *render);
 
-	[[nodiscard]] virtual bool IsFPSLimited() const { return true; }
+    virtual ~GameState() = default;
 
-	virtual void Init() {}
-	virtual void UnInit() {}
+    [[nodiscard]] virtual bool IsFPSLimited() const { return true; }
 
-	virtual void Events(const u32 frame, const u32 totalMSec, const float deltaT){
+    virtual void Init() {}
+
+    virtual void UnInit() {}
+
+    virtual void Events(const u32 frame, const u32 totalMSec, const float deltaT) {
         SDL_PumpEvents();
 
         Event event;
-        while (SDL_PollEvent(&event))
-        {
+        while (SDL_PollEvent(&event)) {
             if (game.HandleEvent(event))
                 continue;
         }
     }
-	virtual void Update(const u32 frame, const u32 totalMSec, const float deltaT) = 0;
-	virtual void Render(const u32 frame, const u32 totalMSec, const float deltaT) = 0;
+
+    virtual void Update(const u32 frame, const u32 totalMSec, const float deltaT) = 0;
+
+    virtual void Render(const u32 frame, const u32 totalMSec, const float deltaT) = 0;
 };
