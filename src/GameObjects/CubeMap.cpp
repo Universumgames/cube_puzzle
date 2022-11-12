b07914c4405ddbf72299d2ac717dc7fb55a3682f
Universumgames <programming@universegame.de>
2022-11-12 20:13:59 +0100

    formatting and add border of minimap to popup when in front of cube map

--- src/GameObjects/CubeMap.cpp
+++ src/GameObjects/CubeMap.cpp
@@ -32,34 +32,45 @@ void CubeMap::RenderUI(const u32 frame, const u32 totalMSec, const float deltaT)
     debugDiceData->RenderUI(BASIC_GO_DATA_PASSTHROUGH);
 }
 
-void sAndNRotation(int w, int c, int e, int b, int n, int s, DiceSideRotation* nRot, DiceSideRotation* sRot){
-    if(nRot == nullptr || sRot == nullptr) return;
+void sAndNRotation(int w, int c, int e, int b, int n, int s, DiceSideRotation *nRot, DiceSideRotation *sRot) {
+    if (nRot == nullptr || sRot == nullptr) return;
     auto anchorN = DiceData::getAnchorSideOfTopLeftCorner(n);
     auto anchorS = DiceData::getAnchorSideOfTopLeftCorner(s);
-    if(anchorN == w) *nRot = DiceSideRotation::LEFT;
-    else if(anchorN == c) *nRot = DiceSideRotation::DOWN;
-    else if(anchorN == e) *nRot = DiceSideRotation::RIGHT;
-    else if(anchorN == b) *nRot = DiceSideRotation::UP;
+    if (anchorN == w) *nRot = DiceSideRotation::LEFT;
+    else if (anchorN == c) *nRot = DiceSideRotation::DOWN;
+    else if (anchorN == e) *nRot = DiceSideRotation::RIGHT;
+    else if (anchorN == b) *nRot = DiceSideRotation::UP;
 
-    if(anchorS == w) *sRot = DiceSideRotation::LEFT;
-    else if(anchorS == c) *sRot = DiceSideRotation::UP;
-    else if(anchorS == e) *sRot = DiceSideRotation::RIGHT;
-    else if(anchorS == b) *sRot = DiceSideRotation::DOWN;
+    if (anchorS == w) *sRot = DiceSideRotation::LEFT;
+    else if (anchorS == c) *sRot = DiceSideRotation::UP;
+    else if (anchorS == e) *sRot = DiceSideRotation::RIGHT;
+    else if (anchorS == b) *sRot = DiceSideRotation::DOWN;
 }
 
 void CubeMap::drawMinimap(const u32 frame, const u32 totalMSec, const float deltaT) {
     minimapText->RenderUI(BASIC_GO_DATA_PASSTHROUGH);
     int c = currentSideId, n = 0, w = 0, e = 0, s = 0, b = 0;
-    int sideSize = 32;
+    int relativeToWindow = max(game.getWindowSize().x, game.getWindowSize().y) / 20.0;
+    int sideSize = max(relativeToWindow, 32);
     int sideDistance = 10;
     DiceData cloneData = DiceData(diceData);
     diceData.get2DRepresentation(c, &n, &w, &e, &s, &b);
     Point offset = {game.getWindowSize().x - (sideSize * 3 + sideDistance), sideDistance};
+    Color borderColor = black;
+    int borderWidth = 1;
 
     auto nRot = DiceSideRotation::UP;
     auto sRot = DiceSideRotation::UP;
-    sAndNRotation(w,c,e,b, n,s, &nRot, &sRot);
+    sAndNRotation(w, c, e, b, n, s, &nRot, &sRot);
 
+    // border of y-axis of cross
+    drawColoredFilledRect(render, borderColor,
+                          {offset.x + sideSize - borderWidth, offset.y - borderWidth, sideSize + borderWidth * 2,
+                           sideSize * 4 + borderWidth * 2});
+    // border of x-axis of cross
+    drawColoredFilledRect(render, borderColor,
+                          {offset.x - borderWidth, offset.y + sideSize - borderWidth, sideSize * 3 + borderWidth * 2,
+                           sideSize + borderWidth * 2});
     drawSide(game.getSpriteStorage()->sideSprites[n - 1], render, {offset.x + sideSize, offset.y, sideSize, sideSize},
              nRot);
     drawSide(game.getSpriteStorage()->sideSprites[w - 1], render, {offset.x, offset.y + sideSize, sideSize, sideSize},
@@ -73,7 +84,6 @@ void CubeMap::drawMinimap(const u32 frame, const u32 totalMSec, const float delt
     drawSide(game.getSpriteStorage()->sideSprites[b - 1], render,
              {offset.x + sideSize, offset.y + sideSize * 3, sideSize, sideSize}, cloneData.getDiceSideRotation(b));
 }
-
 
 
 void CubeMap::drawMap(const u32 frame, const u32 totalMSec, const float deltaT) {
