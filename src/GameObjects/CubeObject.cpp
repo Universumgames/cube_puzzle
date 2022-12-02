e35520a3ecb0f2b5d3d2009e03d894a41ca4346a
Mina91 <marina.inokuchi@stud.hn.de>
2022-12-02 12:23:29 +0100

    added some TODOs

--- src/GameObjects/CubeObject.cpp
+++ src/GameObjects/CubeObject.cpp
@@ -13,21 +13,26 @@ void CubeObject::Render(CubeGame& game, Renderer *render, Point size, Point loca
 }
 
 void Stone::Render(CubeGame& game, Renderer *render, Point size, Point location, u32 frame, u32 totalMSec, float deltaT) {
+    // TODO Tom hier pls einen Stein
     SDL_SetRenderDrawColor(render, 100, 100, 255, 255);
     CubeObject::Render(game, render, size, location, frame, totalMSec, deltaT);
 }
 
 void Slider::Render(CubeGame& game, Renderer *render, Point size, Point location, u32 frame, u32 totalMSec, float deltaT) {
+    // TODO Tom hier pls einen Slider, wobei ich noch implementieren muss, in welche Richtung der slidet,
+    //  also vll erstmal noch warten mit dem Sprite, die sollten dann ja auch unterschiedlich aussehen.
     SDL_SetRenderDrawColor(render, 100, 0, 255, 60);
     CubeObject::Render(game, render, size, location, frame, totalMSec, deltaT);
 }
 
 void Magnet::Render(CubeGame& game, Renderer *render, Point size, Point location, u32 frame, u32 totalMSec, float deltaT) {
+    // TODO Tom hier pls einen "Magneten" oder wie auch immer Leo ihn benannt haben will.
     SDL_SetRenderDrawColor(render, 50, 200, 255, 255);
     CubeObject::Render(game, render, size, location, frame, totalMSec, deltaT);
 }
 
 void Flag::Render(CubeGame& game, Renderer *render, Point size, Point location, u32 frame, u32 totalMSec, float deltaT) {
+    // TODO Tom hier pls eine Flagge
     SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
     CubeObject::Render(game, render, size, location, frame, totalMSec, deltaT);
 }
