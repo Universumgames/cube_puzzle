57ee78b21b876526ee4e9152b5ed7c69ab5afd48
Universumgames <programming@universegame.de>
2022-12-15 23:56:49 +0100

    cleanup zipping

--- CMAKE_package.cmake
+++ CMAKE_package.cmake
@@ -1,18 +1,3 @@
-message(STATUS "post")
-message(STATUS ${CMAKE_CURRENT_BINARY_DIR})
-execute_process(
-        COMMAND pwd
-        OUTPUT_VARIABLE PWD
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-message(STATUS ${PWD})
-execute_process(
-        COMMAND ls -al
-        OUTPUT_VARIABLE LS
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-message(STATUS ${LS})
-
 # architecture
 EXECUTE_PROCESS( COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE ARCHITECTURE )
 
